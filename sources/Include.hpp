0cb25ee57dac15413f642a5f6b45b18c84587163
Martin Maceovic <maceovic@eset.sk>
2023-10-16 14:32:36 +0200

    Core: update cmake

--- sources/Include.hpp
+++ sources/Include.hpp
@@ -5,12 +5,12 @@
 #include <algorithm>
 #include <execution>
 
-#include "imgui.h"
-#include "entt/entt.hpp"
+#include <imgui.h>
+#include <entt/entt.hpp>
 
 namespace ray
 {
-#include "raylib.h"
+#include <raylib.h>
 #include "rlgl.h"
 #include "raymath.h"
 }
